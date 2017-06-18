401d1459c118e9a2977e39f9f56a67cb58cab583
marcogomesr <marcogomesr@gmail.com>
2017-06-19 00:17:34 +0100

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  items = ["Angular", "React", "Underscore"];
+
+  newItem: string = "";
+
+  pushItem(){
+  	if (this.newItem != "") {
+  		this.items.push(this.newItem);
+		  this.newItem = "";
+  	}
+  }
+
+  removeItem(i){
+  	this.items.splice(i, 1);
+  }
 }
